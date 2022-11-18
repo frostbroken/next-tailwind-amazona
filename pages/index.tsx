e592a8afdd0b2d5951d4e28686dc28641f282572
frostbroken <markalvin.oblefias.mao@gmail.com>
2022-11-18 17:02:40 +0800

    newly uploaded files for ecommerce store

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     
+    
     <h1 className='text-3xl font-bold'> Next Tailwind Amazona</h1>
- 
+   
     </div>
   )
 }
